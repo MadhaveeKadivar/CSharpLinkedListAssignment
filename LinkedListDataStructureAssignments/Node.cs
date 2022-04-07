ae318981d8f1c79ec9d57a90fa926585cb6cfc7c
MadhaveeKadivar <100771918+MadhaveeKadivar@users.noreply.github.com>
2022-04-07 07:45:05 +0530

    Update Node.cs

--- LinkedListDataStructureAssignments/Node.cs
+++ LinkedListDataStructureAssignments/Node.cs
@@ -8,9 +8,9 @@ namespace LinkedListDataStructureAssignments
 {
     internal class Node<T>
     {
-        public T data; // Adata variable
+        public T data; // data variable
         public Node<T> next; // declaring a next node
-        public Node(T data) // Cratinf constructor havind node data as parameter
+        public Node(T data) // Creating a constructor having node data as parameter
         {
             this.data = data;
         }
